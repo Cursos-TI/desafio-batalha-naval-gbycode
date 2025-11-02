cbfa6b863651974c95381bba58883a6b96005736
gbycode <gabrielryanrcarvalho24@gmail.com>
2025-11-02 15:35:50 +0000

    TABULEIRO BATALHA NAVAL AVENTUREIRO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,15 +6,23 @@ int main() {
     int tabuleiro[10][10];
     int navioHorizontal [3];
     int navioVertical[3];
+    int navioDiagonal1[3];
+    int navioDiagonal2[3];
     int i, j;
     int linhaH, colunaH;
     int linhaV, colunaV;
+    int linhaD1, colunaD1;
+    int linhaD2, colunaD2;
 
     //Inicialização das variáveis
-    linhaH = 2; //Linha inicial do navio horizontal
+    linhaH = 1; //Linha inicial do navio horizontal
     linhaV = 5; //Linha inicial do navio vertical
-    colunaH = 4; //coluna inicial do navio horizontal
+    colunaH = 2; //coluna inicial do navio horizontal
     colunaV = 7; //coluna inicial do navio vertical
+    linhaD1 = 3; //Linha inicial do navio diagonal
+    linhaD2 = 6; //Linha inicial do navio diagonal
+    colunaD1 = 3; //coluna inicial do navio diagonal
+    colunaD2 = 5; //coluna inicial do navio diagonal
 
     //Inicialização do tabuleiro
     for (int i = 0; i < 10; i++) {
@@ -27,6 +35,8 @@ int main() {
     for (i = 0; i < 3; i++){
         navioHorizontal[i] = 3; //Valor 3 representa o navio
         navioVertical[i] = 3;
+        navioDiagonal1[i] = 3;
+        navioDiagonal2[i] = 3;
     }
 
     //Posicionamento dos navios
@@ -45,11 +55,26 @@ int main() {
         }
     }
 
-    //Exibição do tabuleiro
-    printf("TABULEIRO BATALHA NAVAL NOVATO\n\n");
+    //Navio Diagonal Princiapal
+    for (i = 0; i < 3; i++){
+        if (linhaD1 + i < 10 && colunaD1 + i < 10){
+            tabuleiro[linhaD1 + i][colunaD1 + i] = navioDiagonal1[i];
+        }
+    }
+    
+    //Navio Diagonal Secundária
+    for (i = 0; i < 3; i++){
+        if (linhaD2 + i < 10 && colunaD2 - i >= 0){
+            tabuleiro[linhaD2 + i][colunaD2 - i] = navioDiagonal2[i];
+        }
+    }
 
-    for (i =0; i < 10; i++) {
-        for (j = 0; j< 10; j++){
+
+    //Exibição do tabuleiro
+    printf("TABULEIRO BATALHA NAVAL AVENTUREIRO\n\n");
+
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++){
             printf("%d ", tabuleiro[i][j]); //mostra a posição das matriz
         }
         printf("\n");
